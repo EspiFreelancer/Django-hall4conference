e327105c8b2f5e81e975769fe33cb8549ac00e18
EspiFreelancer <espifreelancer@gmail.com>
2019-05-11 13:37:31 -0300

    Show two forms in one form (template)

--- appHall/forms.py
+++ appHall/forms.py
@@ -1,8 +1,14 @@
 from django import forms
 
-from .models import User
+from .models import User, Owner
 
 class UserRegisterForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ('first_name', 'last_name', 'email', 'password', 'country', 'state',)
+
+class isOwnerForm(forms.ModelForm):
+    class Meta:
+        model = Owner
+        fields = ('is_owner', 'name_company',)
+    
